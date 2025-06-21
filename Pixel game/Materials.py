f04bca58cdf6c08a3e6bc3475edf1cb4efffb406
FlintStone69 <scribzv3@gmail.com>
2025-06-21 02:14:03 -0500

    Add files via upload

---
+++ Pixel game/Materials.py
@@ -0,0 +1,16 @@
+from Colors import *
+
+
+class Material:
+    def __init__(self, Name: str, Color: tuple, Weight: int, fall: bool):
+        self.Type = Name
+        self.Color = Color
+        self.Weight = Weight
+        self.FallB = fall
+
+
+Materials = []
+
+
+def AddMaterial2List(function):
+    Materials.append(function)
